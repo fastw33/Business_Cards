ca57f690898de005ecd58742fe3c1a02dff5171b
Ebernal <Ed2902@users.noreply.github.com>
2026-09-03 09:00:13 -0500

    Add stable Greenway advisory route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, Navigate, useLocation } from 'react-router-dom'
 import CardsPage from './pages/Cards'
 import AsesoresPage from './pages/Asesores'
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<CardsPage />} />
+      <Route path='/' element={<HomeRoute />} />
       <Route path='/cards/:slug' element={<CardsPage />} />
       <Route path='/asesores' element={<AsesoresPage />} />
       <Route
@@ -14,19 +14,47 @@ export default function App() {
         element={<AsesoresPage variante='greenway' />}
       />
       <Route
+        path='/asesorias-greenway'
+        element={<AsesoresPage variante='greenway' />}
+      />
+      <Route
         path='*'
-        element={
-          <div className='p-6'>
-            <p>
-              Ruta no encontrada. Volver al{' '}
-              <Link to='/' className='underline'>
-                inicio
-              </Link>
-              .
-            </p>
-          </div>
-        }
+        element={<NotFoundRoute />}
       />
     </Routes>
+  )
+}
+
+function HomeRoute() {
+  const location = useLocation()
+
+  if (location.hash === '#/asesores#greemway') {
+    return <AsesoresPage variante='greenway' />
+  }
+
+  if (location.hash === '#asesores-greenway') {
+    return <Navigate to='/asesorias-greenway' replace />
+  }
+
+  return <CardsPage />
+}
+
+function NotFoundRoute() {
+  const location = useLocation()
+
+  if (location.hash === '#/asesores#greemway') {
+    return <AsesoresPage variante='greenway' />
+  }
+
+  return (
+    <div className='p-6'>
+      <p>
+        Ruta no encontrada. Volver al{' '}
+        <Link to='/' className='underline'>
+          inicio
+        </Link>
+        .
+      </p>
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
 // main.jsx
 import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/index.css'
 
 /* AOS */
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+
+const LOCKED_GREENWAY_ADVISORS_HASH = '#/asesores#greemway'
+
+function normalizeLegacyHashRoute() {
+  const { hash } = window.location
+
+  if (hash === LOCKED_GREENWAY_ADVISORS_HASH) return
+
+  if (hash === '#asesores-greenway' || hash === '#/asesores-greenway') {
+    window.history.replaceState(null, '', '/asesorias-greenway')
+    return
+  }
+
+  if (hash.startsWith('#/')) {
+    window.history.replaceState(null, '', hash.slice(1))
+  }
+}
 
 export default function Root() {
   useEffect(() => {
@@ -21,11 +38,12 @@ export default function Root() {
 
   return (
     <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   )
 }
 
+normalizeLegacyHashRoute()
 ReactDOM.createRoot(document.getElementById('root')).render(<Root />)
